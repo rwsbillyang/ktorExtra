309e8bb6725c36af3eac809667c461093914b205
BillYang <rwsbillyang@qq.com>
2021-02-19 14:49:58 +0800

    fix time parse and format timezone problem

--- src/util/DatetimeUtil.kt
+++ src/util/DatetimeUtil.kt
@@ -10,12 +10,17 @@ import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
 
 fun LocalDateTime.toUtc() = toInstant(ZoneOffset.UTC).toEpochMilli()
-fun Long.utcToLocalDateTime(): LocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(this),ZoneOffset.UTC)
+fun Long.utcToLocalDateTime(zoneOffset: ZoneOffset = ZoneOffset.UTC): LocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(this),zoneOffset)
 fun Long.plusTime(years: Long = 0L, months: Long = 0L, days: Long = 0L) =
     utcToLocalDateTime().plusYears(years).plusMonths(months).plusDays(days).toUtc()
 
 object DatetimeUtil {
     private val log = LoggerFactory.getLogger("DatetimeUtil")
+    /**
+     * 1天的毫秒数
+     * */
+    const val msOneDay = 24*3600*1000L
+
     /**
      * 计算当前时间的前days天的毫秒起始值. [start,end)
      * based on java8 time package
@@ -35,7 +40,7 @@ object DatetimeUtil {
     /**
      * yyyy-MM-dd hh:mm:ss
      * */
-    fun parse(dateTime: String, format: String = "yyyy-MM-dd hh:mm:ss"): LocalDateTime?{
+    fun parse(dateTime: String, format: String = "yyyy-MM-dd HH:mm:ss"): LocalDateTime?{
         return try {
             LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern(format))
         }catch (e: DateTimeParseException){
@@ -44,5 +49,5 @@ object DatetimeUtil {
         }
     }
 
-    fun format(time: Long, format: String = "yyyy-MM-dd hh:mm:ss") = time.utcToLocalDateTime().format(DateTimeFormatter.ofPattern(format))
+    fun format(time: Long, format: String = "yyyy-MM-dd HH:mm:ss", zoneOffset: ZoneOffset = ZoneOffset.ofHours(8)) = time.utcToLocalDateTime(zoneOffset).format(DateTimeFormatter.ofPattern(format))
 }
