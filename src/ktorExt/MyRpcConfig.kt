ddd773cb7f28020580e28a002203498a94581547
BillYang <rwsbillyang@qq.com>
2021-03-07 15:08:41 +0800

    Use current relative path for SOFA RPC, fix prod/dev run on same machine

--- src/ktorExt/MyRpcConfig.kt
+++ src/ktorExt/MyRpcConfig.kt
@@ -1,17 +1,16 @@
 package com.github.rwsbillyang.ktorExt
 
 import com.alipay.sofa.rpc.common.RpcConstants
-import java.io.File
+
 
 object MyRpcConfig {
     //const val SOFA_REGISTRY_PROTOCOL = RpcConstants.REGISTRY_PROTOCOL_ZK
     var SOFA_REGISTRY_PROTOCOL = RpcConstants.REGISTRY_PROTOCOL_LOCAL
 
-    const val SOFA_REGISTRY_ADDRESS = "127.0.0.1:2181"
-    //var SOFA_REGISTRY_ADDRESS = "local:///Users/bill/git/youke/zkdata/localRegistry"
+    var SOFA_REGISTRY_ADDRESS = "127.0.0.1:2181"
 
-
-    var SOFALocalRegistry = System.getProperty("user.home") + File.separator + "localRegistry"
+    //var SOFALocalRegistry = System.getProperty("user.home") + File.separator + "localRegistry"
+    var SOFALocalRegistry = "./localRegistry" //改为当前目录下, 避免prod和dev 共用产生数据交叉行为
 
 
     /**
